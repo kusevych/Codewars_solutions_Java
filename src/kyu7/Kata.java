cab165a6bf4145f51f2e11cc848335917af7c7ea
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-06-14 14:54:38 -0400

    "Highest and Lowest"

--- src/kyu7/Kata.java
+++ src/kyu7/Kata.java
@@ -16,8 +16,6 @@ All numbers are valid Int32, no need to validate them.
 There will always be at least one number in the input string.
 Output string must be two numbers separated by a single space, and highest number is first.
  */
-
-
 package kyu7;
 
 import java.util.Arrays;
