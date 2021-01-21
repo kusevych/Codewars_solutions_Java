7bbc9740bb99670c451376b4a27e1ba11867275f
Anzhelika Kusevych <lika.kusevich@gmail.com>
2021-01-20 19:36:56 -0500

    "Fake Binary"

--- src/kyu8/FakeBinary.java
+++ src/kyu8/FakeBinary.java
@@ -20,3 +20,9 @@ public class FakeBinary {
         return res;
     }
 }
+
+class FakeBinary1 {
+    public static String fakeBin(String numberString) {
+        return numberString.replaceAll("[0-4]", "0").replaceAll("[5-9]", "1");
+    }
+}
