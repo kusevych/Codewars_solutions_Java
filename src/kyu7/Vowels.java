c58a706f2c8848b03366b5a27dc120640a39b9c8
Anzhelika Kusevych <lika.kusevich@gmail.com>
2021-02-11 09:36:21 -0500

    "Vowel Count"

--- src/kyu7/Vowels.java
+++ src/kyu7/Vowels.java
@@ -29,3 +29,16 @@ class Vowels2 {
         return str.replaceAll("[^aeiou]","").length();
     }
 }
+
+class Vowels3 {
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+        String vowels = "aeiou";
+        for (int i = 0; i < str.length(); i++) {
+            if (vowels.contains(String.valueOf(str.charAt(i)))) {
+                vowelsCount++;
+            }
+        }
+        return vowelsCount;
+    }
+}
