9a091dd46d085cb3f69262d1868d20ae7f255383
Anzhelika Kusevych <lika.kusevich@gmail.com>
2021-04-26 15:31:49 -0400

    "Opposites Attract"

--- src/kyu8/OppositesAttract.java
+++ src/kyu8/OppositesAttract.java
@@ -11,7 +11,7 @@ they aren't.
 
 package kyu8;
 
-public class OppositeAbstract {
+public class OppositesAttract {
     public static boolean isLove(final int flower1, final int flower2) {
         return (flower1 + flower2) % 2 != 0;
     }
@@ -20,5 +20,11 @@ public class OppositeAbstract {
 class OppositesAttract2 {
     public static boolean isLove(final int flower1, final int flower2) {
         return flower1 % 2 != flower2 % 2;
+    }
+}
+
+class OppositesAttract3 {
+    public static boolean isLove(final int flower1, final int flower2) {
+        return (flower1 % 2 == 0 && flower2 % 2 == 1) || (flower1 % 2 == 1 && flower2 % 2 == 0);
     }
 }
