31412286351b6c9a13b0cdb2a0ecdab97fd1662d
Anzhelika Kusevych <lika.kusevich@gmail.com>
2021-05-18 13:17:30 -0400

    "Triple Trouble"

--- src/kyu8/Kata5.java
+++ src/kyu8/Kata5.java
@@ -22,3 +22,15 @@ public class Kata5 {
         return result;
     }
 }
+
+class Kata5a {
+    public static String tripleTrouble(String one, String two, String three) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < one.length(); i++) {
+            result.append(one.charAt(i));
+            result.append(two.charAt(i));
+            result.append(three.charAt(i));
+        }
+        return result.toString();
+    }
+}
